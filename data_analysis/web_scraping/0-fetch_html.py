4dd3be7db19dcb7dc78feb1db597577bba7c826d
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-13 23:00:37 +0200

    Update 0-fetch_html.py

--- data_analysis/web_scraping/0-fetch_html.py
+++ data_analysis/web_scraping/0-fetch_html.py
@@ -3,8 +3,8 @@ import requests
 """Module 0"""
 
 
-def fetch_html(url, headers = None, timeout = 10):
+def fetch_html(url, headers=None, timeout=10):
     """fetches a web page and returns its HTML as text"""
-    response = requests.get(url, headers=headers, timeout= timeout)
+    response = requests.get(url, headers=headers, timeout=timeout)
     response.raise_for_status()
     return response.text
