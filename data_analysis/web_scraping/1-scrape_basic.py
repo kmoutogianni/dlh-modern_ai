4b9aee2261b8b4d2c2d39808ecf72be76bcedc0d
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-14 00:21:15 +0200

    Update 1-scrape_basic.py

--- data_analysis/web_scraping/1-scrape_basic.py
+++ data_analysis/web_scraping/1-scrape_basic.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """Module 1"""
 
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 fetch_html = __import__('0-fetch_html').fetch_html
 
 
 def scrape_basic(url):
-  """scrapes the first page of quotes from quotes.toscrape.com"""
-  html_page = fetch_html(url)
-  soup = BeautifulSoup(html_page)
-  
-  quote_block = soup.find_all('div', class_='quote')
-  quotes = []
+    """scrapes the first page of quotes from quotes.toscrape.com"""
+    html_page = fetch_html(url)
+    soup = BeautifulSoup(html_page)
 
-  for quote in quote_block:
-    text = quote.find('span', class_='text').text.strip()
-    author = quote.find('small', class_='author').text.strip()
-    tag_list = quote.find_all('a', class_='tag')
-    tags = []
-    for tag in tag_list:
-        tags.append(tag.text.strip())
-    quotes.append({'text' : text, 'author' : author, 'tags' : tags}) 
-  return quotes
+    quote_block = soup.find_all('div', class_='quote')
+    quotes = []
+
+    for quote in quote_block:
+        text = quote.find('span', class_='text').text.strip()
+        author = quote.find('small', class_='author').text.strip()
+        tag_list = quote.find_all('a', class_='tag')
+        tags = []
+        for tag in tag_list:
+            tags.append(tag.text.strip())
+        quotes.append({'text': text, 'author': author, 'tags': tags}) 
+    return quotes
