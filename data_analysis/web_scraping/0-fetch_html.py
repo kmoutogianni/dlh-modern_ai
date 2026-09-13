aae7345b1378fb8d1fe5147994b65a64698983de
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-13 22:47:55 +0200

    Update 0-fetch_html.py

--- data_analysis/web_scraping/0-fetch_html.py
+++ data_analysis/web_scraping/0-fetch_html.py
@@ -1,3 +1,6 @@
 import requests
 
 def fetch_html(url, headers = None, timeout = 10):
+    response = requests.get(url, headers=headers, timeout= timeout)
+    response.raise_for_status()
+    return response.text
