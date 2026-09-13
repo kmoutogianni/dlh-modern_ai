adc5ff82f4553302a6aec9c3831391f79465a5f6
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-13 23:47:47 +0200

    Create 1-scrape_basic.py

---
+++ data_analysis/web_scraping/1-scrape_basic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module 1"""
+
+from bs4 import BeautifulSoup 
+fetch_html = __import__('0-fetch_html').fetch_html
+
+
+def scrape_basic(url):
+  """scrapes the first page of quotes from quotes.toscrape.com"""
+  html_page = fetch_html(url)
+  soup = BeautifulSoup(html_page)
+  
+  quote_block = soup.find_all('div', class_='quote')
+  quotes = []
+
+  for quote in quote_block:
+    text = quote[0].find('span', class_='text').text.strip()
+    author = quote[0].find('small', class_='text').text.strip()
+    tags = quote.find_all('a', class_='tag').text.strip()
+    quotes.append({'text' = text, 'author' = author, 'tags'= tages}) 
+  return quotes
