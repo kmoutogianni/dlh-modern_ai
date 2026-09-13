b0b8f1633fa688c562ea7dd88edfad78365a5ea2
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-14 00:25:37 +0200

    Update 2-scrape_paginated.py

--- data_analysis/web_scraping/2-scrape_paginated.py
+++ data_analysis/web_scraping/2-scrape_paginated.py
@@ -2,7 +2,10 @@
 """Module 2"""
 
 from bs4 import BeautifulSoup
+import time
+from urllib import parse
 fetch_html = __import__('0-fetch_html').fetch_html
+scrape_basic = __import__('1-scrape_basic').scrape_basic
 
 
 def def scrape_paginated(base_url):
