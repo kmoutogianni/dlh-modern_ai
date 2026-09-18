594ce5784cba92187495991a1221ab9ee443006e
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-09-18 12:12:15 +0200

    Update 3-scrape_via_api.py

--- data_analysis/web_scraping/3-scrape_via_api.py
+++ data_analysis/web_scraping/3-scrape_via_api.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """Module 3"""
-import json 
+
+import json
 fetch_html = __import__('0-fetch_html').fetch_html
 
 
 def scrape_via_api(base_url):
     """fetches quote data from all the quotes' API pages"""
     page_no = 1
-    url = f"{base_url}/api/quotes?page={page_no}"
     result = []
-    while True:
+    while True:    
+        url = f"{base_url}/api/quotes?page={page_no}"
         response = fetch_html(url)
         quotes = response.json()
         if not quotes:
